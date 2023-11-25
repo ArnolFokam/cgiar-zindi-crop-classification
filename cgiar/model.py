f49bbd2d5b6cd4c40e27e220eb1caab05d2ea6da
Arnol Fokam <arnollfokam23@gmail.com>
2023-11-25 07:02:39 +0200

    new results

--- cgiar/model.py
+++ cgiar/model.py
@@ -6,20 +6,20 @@ import torch.nn as nn
 class Resnet50_V1(nn.Module):
     def __init__(self):
         super(Resnet50_V1, self).__init__()
-        # Load a pretrained CNN (e.g., ResNet50)
-        self.cnn = timm.create_model(
+        
+        self.model = timm.create_model(
             'resnet50.a1_in1k',
             pretrained=True
         )
         
         # Modify the final classification layer 
         # to output a single regression value
-        num_features = self.cnn.fc.in_features
-        self.cnn.fc = nn.Sequential(
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
             nn.Linear(num_features, 512),
             nn.ReLU(),
             nn.Linear(512, 5),
         )
 
     def forward(self, x):
-        return self.cnn(x)
+        return self.model(x)
