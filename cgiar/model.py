aba20ede59450b8eaa5824b2d7185277a02a35f8
Arnol Fokam <arnolfokam23@gmail.com>
2023-11-26 12:42:34 +0200

    addd timm model

--- cgiar/model.py
+++ cgiar/model.py
@@ -22,3 +22,24 @@ class Resnet50_V1(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+    
+class TIMM_Model(nn.Module):
+    def __init__(self, model_name, pretrained=True):
+        super(TIMM_Model, self).__init__()
+        
+        self.model = timm.create_model(
+            model_name,
+            pretrained=pretrained
+        )
+        
+        # Modify the final classification layer 
+        # to output a single regression value
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Sequential(
+            nn.Linear(num_features, 512),
+            nn.ReLU(),
+            nn.Linear(512, 5),
+        )
+
+    def forward(self, x):
+        return self.model(x)
