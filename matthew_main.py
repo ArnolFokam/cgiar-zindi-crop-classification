d030c7fbc2007e0852e0277501caa624140d86a5
Matthew <mk6420@gmail.com>
2023-11-28 10:41:06 +0200

    trying out a conv2 net

---
+++ matthew_main.py
@@ -0,0 +1,9 @@
+from cgiar.utils import time_activity
+
+# choose what you want to run from
+# `solutions.manuel` or solutions.matthew
+from solutions.matthew.v10.run import run
+
+if __name__ == "__main__":
+    with time_activity("Training & Submission"):
+        run()
